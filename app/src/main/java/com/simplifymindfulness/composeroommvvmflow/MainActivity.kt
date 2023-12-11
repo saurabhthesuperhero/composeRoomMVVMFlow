e4c5cd2ebd0cee2719fdaf5c99332f2b074aa32b
saurabhjadhav <saurabh@merchantech.com>
2023-12-11 14:04:54 +0530

    Initial commit

---
+++ app/src/main/java/com/simplifymindfulness/composeroommvvmflow/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.simplifymindfulness.composeroommvvmflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.simplifymindfulness.composeroommvvmflow.ui.theme.ComposeRoomMVVMFlowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeRoomMVVMFlowTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                }
+            }
+        }
+    }
+}
+
